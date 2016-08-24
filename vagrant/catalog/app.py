bdf0356f86093a19ecd57ab47a401aaf354647a5
Luke Epp <lucasfepp@gmail.com>
2016-08-24 09:04:54 -0700

    Added industry.html page

--- vagrant/catalog/app.py
+++ vagrant/catalog/app.py
@@ -14,12 +14,24 @@ session = DBSession()
 
 
 @app.route('/')
-def get():
+@app.route('/industry/')
+def index():
     industries = session.query(Industry)
     companies = session.query(Company)
     return render_template(
         'index.html',
         industries=industries,
+        companies=companies
+    )
+
+
+@app.route('/industry/<int:industry_id>')
+def industry(industry_id):
+    industry = session.query(Industry).filter_by(id=industry_id)
+    companies = session.query(Company).filter_by(industry_id=industry_id)
+    return render_template(
+        'industry.html',
+        industry=industry,
         companies=companies
     )
 
