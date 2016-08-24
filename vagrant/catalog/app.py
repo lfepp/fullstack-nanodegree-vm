6e3885f1a86634306bed067eb10ccc3d6162bf82
Luke Epp <lucasfepp@gmail.com>
2016-08-23 20:11:59 -0700

    WIP index.html

--- vagrant/catalog/app.py
+++ vagrant/catalog/app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Industry, Company
+
 
 app = Flask(__name__)
 
@@ -14,12 +15,28 @@ session = DBSession()
 
 @app.route('/')
 def get():
-    industry = session.query(Industry).first()
-    companies = session.query(Company).filter_by(industry_id=industry.id)
-    output = ''
-    for company in companies:
-        output += "{company_name}</br>".format(company_name=company.name)
-    return output
+    industries = session.query(Industry)
+    companies_by_industry = {}
+    for industry in industries:
+        companies = session.query(Company).filter_by(industry_id=industry.id)
+        companies_by_industry[industry.name] = companies
+    return render_template(
+        'index.html',
+        industries=industries,
+        companies=companies_by_industry
+    )
+        # for company in companies:
+        #     return render_template(
+        #         'company_module.html',
+        #         industry=industry,
+        #         companies=companies
+        #     )
+    # industry = session.query(Industry).first()
+    # companies = session.query(Company).filter_by(industry_id=industry.id)
+    # output = ''
+    # for company in companies:
+    #     output += "{company_name}</br>".format(company_name=company.name)
+    # return output
 
 if __name__ == '__main__':
     app.debug = True
